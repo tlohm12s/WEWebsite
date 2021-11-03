661ee7c92595d8fdde7a6a9846edb138f3ae9a62
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2021-11-03 21:35:31 +0100

    Added exercise 5.3

---
+++ excersise/11/topsort.js
@@ -0,0 +1,84 @@
+let dependencies = [];
+
+function addDependency(from, to) {
+    dependencies.push([from, to]);
+
+    let bullet_point = document.createElement("li");
+    bullet_point.appendChild(document.createTextNode(from + " -> " + to));
+
+    let list = document.getElementById("relation_list");
+    list.appendChild(bullet_point);
+}
+
+function Node(name, dependencies) {
+    this.name = name;
+    this.dependencies = dependencies;
+}
+
+function topsort(dependencies) {
+    let object_types = [];
+    let nodes = [];
+    let sorted = [];
+
+    for(const dependency of dependencies) {
+        //Gather all Node Names
+        for(const object_type of dependency) {
+            for(const other_object_type of dependency) {
+                if(!object_types.includes(other_object_type)) {
+                    object_types.push(other_object_type);
+                }
+            }
+
+        }
+
+        //Invert them
+        let tmp = dependency[1];
+        dependency[1] = dependency[0];
+        dependency[0] = tmp;
+    }
+
+    //Create Nodes with their Dependencies
+    for(const object_type of object_types) {
+        let node_dependencies = 0;
+
+        for(const dependency of dependencies) {
+            if(object_type === dependency[0]) {
+                node_dependencies++;
+            }
+        }
+
+        nodes.push(new Node(object_type, node_dependencies));
+    }
+
+    //Sort
+    while(nodes.length > 0) {
+        for(let i = 0; i < nodes.length; i++) {
+            if(nodes[i].dependencies === 0) {
+                sorted.push(nodes[i].name);
+                nodes.splice(nodes.indexOf(nodes[i]), 1);
+            } else {
+                nodes[i].dependencies--;
+            }
+        }
+    }
+
+    return sorted;
+}
+
+function sort() {
+    document.getElementById("sorted_list").innerHTML = "";
+    let sorted = topsort(dependencies);
+
+    for(let i = 0; i < sorted.length; i++) {
+        let bullet_point = document.createElement("li");
+        bullet_point.appendChild(document.createTextNode(sorted[i]));
+    
+        let list = document.getElementById("sorted_list");
+        list.appendChild(bullet_point);
+    }
+}
+
+function clearList() {
+    dependencies = [];
+    document.getElementById("relation_list").innerHTML = "";
+}
