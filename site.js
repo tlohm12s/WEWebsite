c08a7fdae0e2b518fe01a33a77158e3fad6b7611
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2022-01-17 19:02:05 +0100

    Improved layout and automate content listing

---
+++ site.js
@@ -0,0 +1,26 @@
+(async _ => {
+
+    window.onload = async _ => {
+
+        let navigation = document.getElementById("navigation");
+        let links = await fetch("links.json").then(response => response.json());
+
+        for (const titel in links){
+            let link = links[titel];
+            
+            let nav_item = document.createElement("a");
+            nav_item.classList.add("nav-item");
+            nav_item.textContent = titel;
+            nav_item.href = link;
+            
+            let frame = document.createElement("iframe");
+            frame.src = link;
+
+            nav_item.appendChild(frame);
+
+            navigation.appendChild(nav_item);
+        }
+
+    };
+
+})();
