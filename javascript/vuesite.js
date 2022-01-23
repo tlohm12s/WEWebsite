b549aeaff32ac63d467371d361336890364e51c0
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2022-01-23 23:05:22 +0100

    Finish implementation and js refactoring

---
+++ javascript/vuesite.js
@@ -0,0 +1,32 @@
+Vue.component('nav-item', {
+    props: ['titel', 'link', 'beschreibung'],
+    template: `
+        <div class='nav-item'>
+            <a style='font-weight: bold;' :href='link' target='_blank'> 
+                {{titel}} 
+            </a>
+            <p> 
+                <b><u>Aufgabe:</u></b> 
+                <br>
+                {{beschreibung}}
+            </p>
+
+            <iframe :src="link"> </iframe>
+        </div>
+    `
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        navitems: [] 
+    },
+    created: async function() {
+        await fetch("/resources/links.json").then(response => response.json()).then(result => {
+            this.navitems = result;
+        });
+
+        console.log("asd");
+        console.log("Result", this.navitems);
+    }
+});
--- main.php
+++ main.php
@@ -3,13 +3,12 @@
         
         <h1 style="background-color: steelblue; margin: auto; border-radius: 1em; padding: 0.5em; text-align: center; width: fit-content;"> <u>Best contents</u> </h1>
 
-        <nav id="navigation"></nav>
-
         <nav id="app">
-            <nav-item titel="asd" link="asd"></nav-item>
+            <nav-item v-for="navitem in navitems" :key="navitem.titel" :link="navitem.link" :titel="navitem.titel" :beschreibung="navitem.beschreibung"> </nav-item>
         </nav>
+
     </div>
 </section>
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-<script type="text/javascript" src="/javascript/site.js"></script>
+<script type="text/javascript" src="/javascript/vuesite.js"></script>
