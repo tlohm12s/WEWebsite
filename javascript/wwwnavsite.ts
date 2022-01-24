e13ecb7bc6d2769fe81f2296c9c7c29853d5e374
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2022-01-24 18:12:53 +0100

    Fix path

--- javascript/wwwnavsite.ts
+++ javascript/wwwnavsite.ts
@@ -2,7 +2,7 @@ async function run() {
 
     const TYPE_CATEGORY : number = 0, TYPE_SUBCATEGORY : number = 1;
 
-    let categories : Response = await fetch("/resources/navigator_contents.json").then(response => response.json());
+    let categories : Response = await fetch("../resources/navigator_contents.json").then(response => response.json());
 
     let category_list : HTMLElement = document.getElementById("categories");
     let subcategory_list : HTMLElement = document.getElementById("subcategories");
