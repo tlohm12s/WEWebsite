148fd8a3338ede8a7f4671bab6b60848dfcd95f7
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2022-01-24 01:40:22 +0100

    Fix server request

--- www-navigator/server.php
+++ www-navigator/server.php
@@ -1,12 +1,11 @@
 <?php 
     session_start();
-    $accounttoken = "otbMNB38";
 
     //Aus den Folien
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if($_SESSION['token'] === $accounttoken) {
+        if($_SESSION['privileges']) {
             $raw_data = file_get_contents('php://input');
-            $result = file_put_contents('navigator_contents.json', $raw_data);
+            $result = file_put_contents('../resources/navigator_contents.json', $raw_data); 
             if($result === false) { http_response_code(500); } else { http_response_code(200); }
         } else {
             http_response_code(401);
