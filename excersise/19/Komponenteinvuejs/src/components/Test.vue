5f04f0d23eb6202fd77933c34b68edce58686d12
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2022-01-25 21:04:20 +0100

    Quellenangabe

--- excersise/19/Komponenteinvuejs/src/components/Test.vue
+++ excersise/19/Komponenteinvuejs/src/components/Test.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+      <!-- Mit https://regex101.com/ erstellt -->
       <input type="text" v-model="message">
       <br> Anzahl Buchstaben: {{ message.match(/\w/g) == null ? 0 : message.match(/\w/g).length }}
       <br> Anzahl Leerzeichen: {{ message.match(/\s/g) == null ? 0 : message.match(/\s/g).length }}
